5d12f9bf909ee6f5a714842c57e373bb846f1d20
Gabriel Sousa <gabrielsou1912@gmail.com>
2023-08-23 13:42:38 -0300

    new version

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,54 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Products;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-        public function index(){
-            return view('products.index');
-        }
+    public function index(){
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
+        
+    }
 
-        public function create(){
-            return view('products.create');
-        }
+    public function create(){
+        return view('products.create');
+    }
 
-        public function store(Request $request){
-            $data = $request->validate([
-                'name'=>'required',
-                'qty'=>'required|numeric',
-                'price'=>'required|decimal:2',
-                'description' => 'nullable'
-            ]);
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
 
-            $newProduct = Product::create($data);
+        $newProduct = Product::create($data);
 
-            return redirect(route('product.index'));
+        return redirect(route('product.index'));
 
-        }
+    }
 
-        public function edit(Product $product){
+    public function edit(Product $product){
+        return view('products.edit', ['product' => $product]);
+    }
 
-        }
+    public function update(Product $product, Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
 
+        $product->update($data);
 
-        public function update(Product $product, Request $request){
-            $data = $request->validate([
-                'name' => 'required',
-                'qty' => 'required|numeric',
-                'price' => 'required|decimal:0,2',
-                'description' => 'nullable' 
+        return redirect(route('product.index'))->with('success', 'Product Updated Succesffully');
 
-            ]);
-            
-            $product->update($data);
-            
-            return redirect(route('product.index'))->with('success','Product Updated Succesfully');
-        }
+    }
 
-        public function destroy(Product $product){
-            $product->delete();
-            return redirect(route('product.index'))->with('success', 'Product deleted Succesffully');
-}
-
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect(route('product.index'))->with('success', 'Product deleted Succesffully');
+    }
 }
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,42 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <h1>Create the product</h1>
+    <h1>Create a Product</h1>
     <div>
-        @if($erros->any())
+        @if($errors->any())
         <ul>
-           @foreach($erros->all() as $error)
-            <li>{{$error}}</li>
-           @endforeach
+         @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
         </ul>
 
         @endif
     </div>
-    <form action="post" action="{{route('product.store'}}">
-        @csrf
-        @method('post')
-        <div>
-            <label>Name</label>
-            <input type="text" name="name" placeholder="Name">
-        </div>
-        <div>
-            <label>Qty</label>
-            <input type="text" name="qty" placeholder="Qty">
-        </div>
-        <div>
-            <label>Price</label>
-            <input type="text" name="price" placeholder="Price">1
-        </div>
-        <div>
-            <label>Description</label>
-            <input type="text" name="description" placeholder="Description">
-        </div>
-        <div>
-            <input type="submit" value="Save a New Product">
-        </div>
+<form method="post" action="{{route('product.store')}}">
+    @csrf 
+    @method('post')
+    <div>
+        <label>Name</label>
+        <input type="text" name="name" placeholder="Name" />
+    </div>
+    <div>
+        <label>Qty</label>
+        <input type="text" name="qty" placeholder="Qty" />
+    </div>
+    <div>
+        <label>Price</label>
+        <input type="text" name="price" placeholder="Price" />
+    </div>
+    <div>
+        <label>Description</label>
+        <input type="text" name="description" placeholder="Description" />
+    </div>
+    <div>
+        <input type="submit" value="Save a New Product" />
+    </div>
     </form>
 </body>
 </html>
